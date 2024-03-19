f7aef555ccce7298ee554e79110acbaa8e64fcf9
Anamul9901 <anamulhaque9901@gmail.com>
2024-03-19 06:38:41 +0600

    Todo app setup by next.js

---
+++ src/app/(withLayout)/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "Todo",
+  description: "Jobe tasks",
+};
+const Layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
